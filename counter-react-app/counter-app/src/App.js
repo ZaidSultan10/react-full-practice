d62981cf4d56934d57be260dbd0209a7e5c72c99
zaidsultan10 <zaidsultan09@gmail.com>
2024-01-09 23:47:27 +0400

    counter app simple done

--- counter-react-app/counter-app/src/App.js
+++ counter-react-app/counter-app/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const handleIncrement = () => {
+    setCount(count + 1)
+  }
+
+  const handleDecrement = () => {
+    setCount(count - 1)
+  }
   return (
     <div className="App">
-      counter app
+      <button onClick={handleIncrement}>{`+`}</button>
+      <h3>{count}</h3>
+      <button disabled = { count === 0 } onClick={handleDecrement}>{`-`}</button>
     </div>
   );
 }
