da1ca5bc696d296cf02c5f19ea77b709afa6d773
zaidsultan10 <zaidsultan09@gmail.com>
2024-01-14 23:03:11 +0400

    pizza app initial setup

---
+++ react-props-practice/react-props-practice/src/assets/pizzaData.js
@@ -0,0 +1,43 @@
+const pizzaData = [{
+    id : 1,
+    name : `Pepperoni Pizza`,
+    ingredients : `Hand-Crafted Bread, Cheese, Pepperoni`,
+    price : 18,
+    currency : "$",
+    isSoldOut : false,
+},{
+    id : 2,
+    name : `Chicken Tikka Pizza`,
+    ingredients : `Hand-Crafted Bread, Cheese, Spicy Chicken, Onions, Red Bell Pepper`,
+    price : 15,
+    currency : "$",
+    isSoldOut : false,
+},{
+    id : 3,
+    name : `Supreme Pizza`,
+    ingredients : `Hand-Crafted Bread, Cheese, Pepperoni, Grilled Chicken, Olives, Mushroom, Onions`,
+    price : 20,
+    currency : "$",
+    isSoldOut : false,
+},{
+    id : 4,
+    name : `Spicy Ranch Pizza`,
+    ingredients : `Hand-Crafted Bread, Cheese, Ranch Sauce, Grilled Chicken, Olives, Mushroom, Onions`,
+    price : 25,
+    currency : "$",
+    isSoldOut : false,
+},{
+    id : 5,
+    name : `Cheese Lovers Pizza`,
+    ingredients : `Hand-Crafted Bread, Cheese`,
+    price : 12,
+    currency : "$",
+    isSoldOut : false,
+},{
+    id : 6,
+    name : `Huawaiyan Pizza`,
+    ingredients : `Hand-Crafted Bread, Cheese, Pineapple, Grilled Chicken, Bacon, Mushroom, Onions`,
+    price : 16,
+    currency : "$",
+    isSoldOut : false,
+}]
