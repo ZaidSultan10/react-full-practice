5195311c067d9d5297bc1b3db03f9bf4c288bd71
zaidsultan10 <zaidsultan09@gmail.com>
2024-01-28 22:28:29 +0400

    step form complete

--- flash-card/flash-card-app/src/components/StepForm.jsx
+++ flash-card/flash-card-app/src/components/StepForm.jsx
@@ -6,14 +6,26 @@ const StepForm = () => {
     const [increment, setIncrement] = useState(1)
   return (
     <section className='stepForm__main'>
-        <div>
+        <div className='stepForm__top'>
             {
                 data && data.map(elem => (
-                    <div key={elem.id} className='stepForm__main__stepCount'>
+                <div key={elem.id} className={`stepForm__main__stepCount ${increment === elem.id ? `active` : 
+                increment > elem.id ? `complete` : `notAttempted`}`}>
                         <p>{elem.id}</p>
                     </div>
                 ))
             }
+        </div>
+        <div className='stepForm__center'>
+            <p>
+                { 
+                    data && data.filter(item => item.id === increment)[0]?.step
+                }
+            </p>
+        </div>
+        <div className='stepForm__bottom'>
+            <button disabled = {increment === 1} onClick={() => setIncrement(increment - 1)}>{`Previous`}</button>
+            <button disabled = {increment === data.length} onClick={() => setIncrement(increment + 1)}>{`Next`}</button>
         </div>
     </section>
   )
