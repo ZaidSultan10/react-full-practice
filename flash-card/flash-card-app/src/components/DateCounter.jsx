b04a266699e8217667e5a90bf5e4173e9e1f78c5
zaidsultan10 <zaidsultan09@gmail.com>
2024-01-29 23:43:35 +0400

    date counter project finished

--- flash-card/flash-card-app/src/components/DateCounter.jsx
+++ flash-card/flash-card-app/src/components/DateCounter.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
+import moment from 'moment'
 
 const DateCounter = () => {
+    const [counter, setCounter] = useState(0)
+    const [steps, setSteps] = useState(1)
+    
   return (
-    <div>DateCounter</div>
+    <section className='dateCounter'>
+        <div className='dateCounter__counter'>
+            <button disabled = {!counter} onClick={() => counter < steps ? setCounter((steps - (steps - counter)) - counter) : setCounter(counter - steps)}>-</button>
+            <p>{`Count : ${counter}`}</p>
+            <button onClick={() => setCounter(counter + steps)}>+</button>
+        </div>
+        <div className='dateCounter__date'>
+            <h3>
+                {moment().subtract(counter, 'days').utc().format("DD-MMM-YYYY HH:mm:ss")}
+            </h3>
+        </div>
+        <div className='dateCounter__steps'>
+            <button disabled = {steps <= 1} onClick={() => setSteps(steps - 1)}>-</button>
+            <p>{`Steps : ${steps}`}</p>
+            <button onClick={() => setSteps(steps + 1)}>+</button>
+        </div>
+    </section>
   )
 }
 
