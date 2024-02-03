3b87159bc965637d7eb52ca8702f54b690091537
zaidsultan10 <zaidsultan09@gmail.com>
2024-02-03 17:43:49 +0400

    far away island layout

--- far-away-island/far-away-app/src/components/searchBar/SearchBar.jsx
+++ far-away-island/far-away-app/src/components/searchBar/SearchBar.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import './_searchBar.css'
 import SearchBarInput from './searchBarInput/SearchBarInput'
+import { useState } from 'react'
 
 const SearchBar = () => {
+    const [quantity, setQuantity] = useState(0)
+    const [item, setItem] = useState('')
+
+    const handleItemAddition = () => {
+        const data = {
+            quantity,
+            item
+        }
+        setItem('')
+        setQuantity(0)
+        console.log(data)
+    }
   return (
     <section className='searchBar'>
         <div className='searchBar__text'>
             <p>{`What do you need for your trip 🌈`}</p>
         </div>
-        <SearchBarInput type={`text`} classname={`searchBar__input__text`} placeholder={`Enter item...`} />
-        <SearchBarInput type={`number`} classname={`searchBar__input__quantity`} placeholder={``}/>
+        <SearchBarInput type={`text`} classname={`searchBar__input__text`} placeholder={`Enter item...`} value={item} change={setItem} />
+        <SearchBarInput type={`number`} classname={`searchBar__input__quantity`} placeholder={``} value = {quantity} change={setQuantity} />
         <div className='searchBar__button'>
-            <button>{`Add`}</button>
+            <button disabled = {!item || (parseInt(quantity) <= 0)} onClick={handleItemAddition}>{`Add`}</button>
         </div>
     </section>
   )
--- far-away-island/far-away-app/src/components/searchBar/searchBarInput/SearchBarInput.jsx
+++ far-away-island/far-away-app/src/components/searchBar/searchBarInput/SearchBarInput.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const SearchBarInput = ({type, classname, placeholder}) => {
+const SearchBarInput = ({type, classname, placeholder, value, change}) => {
   return (
     <div className={classname}>
-        <input type={type} placeholder={placeholder} />
+        <input type={type} placeholder={placeholder} value={value} onChange={(e) => change(e.target.value)} />
     </div>
   )
 }
