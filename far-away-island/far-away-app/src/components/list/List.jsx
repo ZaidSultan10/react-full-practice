d84fe0b75cc84729ff5591c51660a99419e35e4e
zaidsultan10 <zaidsultan09@gmail.com>
2024-02-04 23:29:59 +0400

    far away island list component

--- far-away-island/far-away-app/src/components/list/List.jsx
+++ far-away-island/far-away-app/src/components/list/List.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './_list.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const List = () => {
   return (
-    <div className='list'>List</div>
+    <section className='list'>
+        <div className='list__actions'>
+            <button>
+                <FontAwesomeIcon icon = {faCoffee} />
+            </button>
+        </div>
+        <div className='list__item'>
+            <input type='checkbox' />
+            <p>2 biscuits</p>
+        </div>
+    </section>
   )
 }
 
